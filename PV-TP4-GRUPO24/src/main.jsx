bcfb1264f54c26b91061e9803fc7e315a91b02cb
GabrielCalisaya <gabrielcalisaya98@gmail.com>
2025-05-15 16:08:01 -0300

    Prueba de errores en GitHub - GabrielCalisaya

--- PV-TP4-GRUPO24/src/main.jsx
+++ PV-TP4-GRUPO24/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Productos from './Productos.jsx'
+import Prueba from './Prueba.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Productos />
+    <Prueba />
   </StrictMode>,
 )
